cec9fd94412d8fe3f71fe11ed04227cb4ebd782d
KEINOS <github@keinos.com>
2020-04-26 16:35:05 +0900

    chore: add space

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@ namespace KEINOS\Sample;
 function getContentsFromStdin()
 {
     $contents = \file_get_contents('php://stdin');
-    if($contents === false){
+    if ($contents === false) {
         throw new \RuntimeException('Failed to read contents from STDIN.');
     }
 
